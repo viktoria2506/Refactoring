479aa725e8bcd5b8b9604449b7e2a1a335c44550
viktoria2506 <vika.chernookaya@mail.ru>
2020-11-01 20:36:03 +0300

    update test

--- src/test/java/ru/akirakozov/sd/refactoring/servlet/QueryServletTest.java
+++ src/test/java/ru/akirakozov/sd/refactoring/servlet/QueryServletTest.java
@@ -2,84 +2,62 @@ package ru.akirakozov.sd.refactoring.servlet;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import ru.akirakozov.sd.refactoring.dao.ProductDao;
+import ru.akirakozov.sd.refactoring.product.Product;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Connection;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import static ru.akirakozov.sd.refactoring.dao.DaoUtils.createTables;
 
 public class QueryServletTest {
-    @Mock
-    private ProductDao productDao;
-
+    private AbstractProductServlet servlet;
+    private final ProductDao productDao = mock(ProductDao.class);
     private final HttpServletRequest request = mock(HttpServletRequest.class);
     private final HttpServletResponse response = mock(HttpServletResponse.class);
     private final StringWriter writer = new StringWriter();
     private final QueryServlet queryServlet = new QueryServlet(productDao);
 
     private static final String DB_ADDRESS = "jdbc:sqlite:test.db";
-    private static final String DB = "insert into product(name, price) values" +
-                        "('bla', '1')," +
-                        "('apple', '0')," +
-                        "('null', '-1')";
-
     private String joinResult(String res) {
         return "<html><body>\n" + res + "</body></html>\n";
-
     }
 
     @BeforeEach
     public void setup() throws IOException, SQLException {
         when(response.getWriter()).thenReturn(new PrintWriter(writer));
-        try (final Connection connection = DriverManager.getConnection(DB_ADDRESS)) {
-            final String query = "drop table if exists product";
-            connection.prepareStatement(query).execute();
-        }
-
-        try (final Connection connection = DriverManager.getConnection(DB_ADDRESS)) {
-            final String query = "create table if not exists product(" +
-                        "id integer primary key autoincrement not null," +
-                        "name text not null," +
-                        "price int not null)";
-            connection.prepareStatement(query).execute();
-        }
+        createTables();
+        servlet = new QueryServlet(productDao);
     }
 
     @Test
     public void testMax() throws IOException, SQLException {
         when(request.getParameter("command")).thenReturn("max");
-        try (final Connection connection = DriverManager.getConnection(DB_ADDRESS)) {
-            connection.prepareStatement(DB).execute();
-        }
-        queryServlet.doGet(request, response);
-        assertEquals(joinResult("<h1>Items with max price: </h1>\nbla	1</br>\n"), writer.toString());
+        when(productDao.findMaxPriceProduct())
+                .thenReturn(Optional.of(new Product("bla", 1)));
+        servlet.doGet(request, response);
+        assertEquals(joinResult("<h1>Product with max price: </h1>\nbla	1</br>\n"), writer.toString());
     }
 
     @Test
     public void testMin() throws IOException, SQLException {
         when(request.getParameter("command")).thenReturn("min");
-        try (final Connection connection = DriverManager.getConnection(DB_ADDRESS)) {
-            connection.prepareStatement(DB).execute();
-        }
+        when(productDao.findMinPriceProduct())
+                .thenReturn(Optional.of(new Product("null", -1)));
         queryServlet.doGet(request, response);
-        assertEquals(joinResult("<h1>Items with min price: </h1>\nnull	-1</br>\n"), writer.toString());
+        assertEquals(joinResult("<h1>Product with min price: </h1>\nnull	-1</br>\n"), writer.toString());
     }
 
     @Test
     public void testSum() throws IOException, SQLException {
         when(request.getParameter("command")).thenReturn("sum");
-        try (final Connection connection = DriverManager.getConnection(DB_ADDRESS)) {
-            connection.prepareStatement(DB).execute();
-        }
         queryServlet.doGet(request, response);
         assertEquals(joinResult(String.format("Summary price: \n0\n")), writer.toString());
     }
@@ -87,23 +65,22 @@ public class QueryServletTest {
     @Test
     public void testCount() throws IOException, SQLException {
         when(request.getParameter("command")).thenReturn("count");
-        try (final Connection connection = DriverManager.getConnection(DB_ADDRESS)) {
-            connection.prepareStatement(DB).execute();
-        }
-        queryServlet.doGet(request, response);
+        when(productDao.getProductsCount())
+                .thenReturn(3);
+        servlet.doGet(request, response);
         assertEquals(joinResult(String.format("Number of products: \n3\n")), writer.toString());
     }
 
     @Test
     public void testQueryUnknownCommand() throws IOException {
         when(request.getParameter("command")).thenReturn("unknown");
-        queryServlet.doGet(request, response);
+        servlet.doGet(request, response);
         assertEquals("Unknown command: unknown\n", writer.toString());
     }
 
     @Test
     public void testQueryNullCommand() throws IOException {
-        queryServlet.doGet(request, response);
+        servlet.doGet(request, response);
         assertEquals("Unknown command: null\n", writer.toString());
     }
 }
