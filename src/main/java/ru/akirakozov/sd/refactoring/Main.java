2cb64d6beb73cbf981ec33a619e620177d42eb14
viktoria2506 <vika.chernookaya@mail.ru>
2020-11-01 17:25:33 +0300

    create dao

--- src/main/java/ru/akirakozov/sd/refactoring/Main.java
+++ src/main/java/ru/akirakozov/sd/refactoring/Main.java
@@ -7,25 +7,14 @@ import ru.akirakozov.sd.refactoring.servlet.AddProductServlet;
 import ru.akirakozov.sd.refactoring.servlet.GetProductsServlet;
 import ru.akirakozov.sd.refactoring.servlet.QueryServlet;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import static ru.akirakozov.sd.refactoring.dao.DaoUtils.createTables;
 
 /**
  * @author akirakozov
  */
 public class Main {
     public static void main(String[] args) throws Exception {
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            String sql = "CREATE TABLE IF NOT EXISTS PRODUCT" +
-                    "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
-                    " NAME           TEXT    NOT NULL, " +
-                    " PRICE          INT     NOT NULL)";
-            Statement stmt = c.createStatement();
-
-            stmt.executeUpdate(sql);
-            stmt.close();
-        }
+        createTables();
 
         Server server = new Server(8081);
 
@@ -34,8 +23,8 @@ public class Main {
         server.setHandler(context);
 
         context.addServlet(new ServletHolder(new AddProductServlet()), "/add-product");
-        context.addServlet(new ServletHolder(new GetProductsServlet()),"/get-products");
-        context.addServlet(new ServletHolder(new QueryServlet()),"/query");
+        context.addServlet(new ServletHolder(new GetProductsServlet()), "/get-products");
+        context.addServlet(new ServletHolder(new QueryServlet()), "/query");
 
         server.start();
         server.join();
---
+++ src/main/java/ru/akirakozov/sd/refactoring/dao/DaoUtils.java
@@ -0,0 +1,32 @@
+package ru.akirakozov.sd.refactoring.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DaoUtils {
+    private static String DB_URL = "jdbc:sqlite:test.db";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL);
+    }
+
+    public static void createTables() throws SQLException {
+        try (Statement stmt = getConnection().createStatement()) {
+            String sql = "CREATE TABLE IF NOT EXISTS PRODUCT" +
+                    "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                    " NAME           TEXT    NOT NULL, " +
+                    " PRICE          INT     NOT NULL)";
+
+            stmt.executeUpdate(sql);
+        }
+    }
+
+    public static void cleanTables() throws SQLException {
+        try (Statement stmt = getConnection().createStatement()) {
+            String sql = "DELETE FROM PRODUCT";
+            stmt.executeUpdate(sql);
+        }
+    }
+}
