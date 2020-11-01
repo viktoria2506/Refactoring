01bdcd276a4a7edc25d70bc0ea9d7227b9ee6135
viktoria2506 <vika.chernookaya@mail.ru>
2020-11-01 18:26:01 +0300

    use AbstractProductServlet

--- src/main/java/ru/akirakozov/sd/refactoring/servlet/AddProductServlet.java
+++ src/main/java/ru/akirakozov/sd/refactoring/servlet/AddProductServlet.java
@@ -9,26 +9,24 @@ import java.sql.SQLException;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
+import ru.akirakozov.sd.refactoring.dao.ProductDao;
+import ru.akirakozov.sd.refactoring.product.Product;
 import ru.akirakozov.sd.refactoring.servlet.*;
 
 /**
  * @author akirakozov
  */
-public class AddProductServlet extends HttpServlet {
+public class AddProductServlet extends AbstractProductServlet {
+    public AddProductServlet(ProductDao productDao) {
+        super(productDao);
+    }
 
     @Override
-    protected void doGet(HttpServletRequest request,
-                         HttpServletResponse response) throws IOException {
+    protected void doRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         String name = request.getParameter("name");
         long price = Long.parseLong(request.getParameter("price"));
 
-        ServletCommon.doGoodies(
-            response,
-            (Statement stmt) -> {
-                String sql = "INSERT INTO PRODUCT " +
-                    "(NAME, PRICE) VALUES (\"" + name + "\"," + price + ")";
-                stmt.executeUpdate(sql);
-            });
+        productDao.addProduct(new Product(name, price));
 
         response.getWriter().println("OK");
     }
