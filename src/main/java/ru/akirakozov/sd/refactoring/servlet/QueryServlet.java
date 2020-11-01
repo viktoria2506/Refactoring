a08819029510a27c9a70dd1a0ebce0b6054d51d2
viktoria2506 <vika.chernookaya@mail.ru>
2020-11-01 18:27:12 +0300

    use AbstractProductServlet in QuaryServlet

--- src/main/java/ru/akirakozov/sd/refactoring/servlet/QueryServlet.java
+++ src/main/java/ru/akirakozov/sd/refactoring/servlet/QueryServlet.java
@@ -1,5 +1,8 @@
 package ru.akirakozov.sd.refactoring.servlet;
 
+import ru.akirakozov.sd.refactoring.dao.ProductDao;
+import ru.akirakozov.sd.refactoring.product.Product;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,45 +12,34 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Optional;
 
 /**
  * @author akirakozov
  */
-public class QueryServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request,
-                         HttpServletResponse response) throws IOException {
+public class QueryServlet extends AbstractProductServlet  {
+    public QueryServlet(ProductDao productDao) {
+        super(productDao);
+    }
 
+    @Override
+    protected void doRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         String command = request.getParameter("command");
 
         if ("max".equals(command)) {
-            ServletCommon.doGoodies(response, (Statement stmt) -> {
-                ResultSet rs =
-                    stmt.executeQuery("SELECT * FROM PRODUCT ORDER BY PRICE DESC LIMIT 1");
-                ServletCommon.dumpItems(response, rs, "Items with max price");});
+            Optional<Product> maxPriceProduct = productDao.findMaxPriceProduct();
+            printProductHTML(maxPriceProduct, "Product with max price: ", response.getWriter());
         } else if ("min".equals(command)) {
-            ServletCommon.doGoodies(response, (Statement stmt) -> {
-                ResultSet rs =
-                    stmt.executeQuery("SELECT * FROM PRODUCT ORDER BY PRICE LIMIT 1");
-                ServletCommon.dumpItems(response, rs, "Items with min price");});
+            Optional<Product> minPriceProduct = productDao.findMinPriceProduct();
+            printProductHTML(minPriceProduct, "Product with min price: ", response.getWriter());
         } else if ("sum".equals(command)) {
-            ServletCommon.doGoodies(response, (Statement stmt) -> {
-                ResultSet rs = stmt.executeQuery("SELECT SUM(price) FROM PRODUCT");
-                response.getWriter().println("<html><body>");
-                response.getWriter().println("Summary price: ");
-                if (rs.next()) response.getWriter().println(rs.getInt(1));
-                response.getWriter().println("</body></html>");});
+            long summaryPrice = productDao.getPricesSum();
+            printInfoHTML(summaryPrice, "Summary price: ", response.getWriter());
         } else if ("count".equals(command)) {
-            ServletCommon.doGoodies(response, (Statement stmt) -> {
-                ResultSet rs = stmt.executeQuery("SELECT COUNT(*) FROM PRODUCT");
-                response.getWriter().println("<html><body>");
-                response.getWriter().println("Number of products: ");
-                if (rs.next()) response.getWriter().println(rs.getInt(1));
-                response.getWriter().println("</body></html>");});
+            int count = productDao.getProductsCount();
+            printInfoHTML(count, "Number of products: ", response.getWriter());
         } else {
             response.getWriter().println("Unknown command: " + command);
-            response.setContentType("text/html");
-            response.setStatus(HttpServletResponse.SC_OK);
         }
     }
 
